685d48605d00499cc451c7fa0400fbc0e9f41ed1
Abenx <5195693+rushairer@users.noreply.github.com>
2021-04-24 12:23:34 +0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,20 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Abenx on 2021/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
